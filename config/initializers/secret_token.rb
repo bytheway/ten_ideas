5c8fc7242a84ec0fadaac69d56605b0705285c11
Ben Bytheway <ben@vrusp.com>
2012-12-03 20:15:49 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TenIdeas::Application.config.secret_token = 'dccc9bb42368790102840475fbb1ec16da2991a906dacf1b21e83caa5a07ff2d814ebdb757fbf211a9beddb31fb02f1d02857320182a4028dc81d4e309312a12'
